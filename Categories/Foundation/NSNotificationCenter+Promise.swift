437b50ecd51bd30e731825ec105421cd918f956e
Volkan Demircin <volkan@demircin.com>
2016-05-15 15:40:58 +0300

    Issue 415 temp fix

--- Categories/Foundation/NSNotificationCenter+Promise.swift
+++ Categories/Foundation/NSNotificationCenter+Promise.swift
@@ -19,33 +19,33 @@ import PromiseKit
     import PromiseKit
 */
 extension NSNotificationCenter {
-    public class func once(name: String) -> NotificationPromise {
+    public class func once(name: String) -> NotificationPromise<[NSObject: AnyObject]> {
         return NSNotificationCenter.defaultCenter().once(name)
     }
-
-    public func once(name: String) -> NotificationPromise {
-        let (promise, fulfill) = NotificationPromise.go()
+    
+    public func once(name: String) -> NotificationPromise<[NSObject: AnyObject]> {
+        let (promise, fulfill) = NotificationPromise<[NSObject: AnyObject]>.go()
         let id = addObserverForName(name, object: nil, queue: nil, usingBlock: fulfill)
         promise.then(on: zalgo) { _ in self.removeObserver(id) }
         return promise
     }
 }
 
-public class NotificationPromise: Promise<[NSObject: AnyObject]> {
+public class NotificationPromise<T>: Promise<T> {
     private let (parentPromise, parentFulfill, _) = Promise<NSNotification>.pendingPromise()
-
+    
     public func asNotification() -> Promise<NSNotification> {
         return parentPromise
     }
-
-    private class func go() -> (NotificationPromise, (NSNotification) -> Void) {
+    
+    private class func go() -> (NotificationPromise<[NSObject: AnyObject]>, (NSNotification) -> Void) {
         var fulfill: (([NSObject: AnyObject]) -> Void)!
-        let promise = NotificationPromise { f, _ in fulfill = f }
+        let promise = NotificationPromise<[NSObject: AnyObject]> { f, _ in fulfill = f }
         promise.parentPromise.then { fulfill($0.userInfo ?? [:]) }
         return (promise, promise.parentFulfill)
     }
-
-    private override init(@noescape resolvers: (fulfill: ([NSObject: AnyObject]) -> Void, reject: (ErrorType) -> Void) throws -> Void) {
+    
+    private override init(@noescape resolvers: (fulfill: (T) -> Void, reject: (ErrorType) -> Void) throws -> Void) {
         super.init(resolvers: resolvers)
     }
 }
